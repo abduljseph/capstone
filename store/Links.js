55854f3d9f8eccc235785101004423210f926152
Abdul Joseph <abduljseph@gmail.com>
2024-03-05 12:09:30 -0600

    Capstone SPA

---
+++ store/Links.js
@@ -0,0 +1,7 @@
+export default [
+  { title: "Home", text: "Home" },
+  { title: "Bio", text: "Bio" },
+  { title: "Order", text: "Submit a Hereyago order" },
+
+  // { title: "Pizza", text: "See all the Pizzas" },
+];
